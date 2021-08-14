063b5514337f3a7b81a8d03bb11158cd71fbd84e
Elnatã Oliveira <elnataoliveira@gmail.com>
2021-08-14 13:21:39 -0300

    Create load_init_jod.sh

---
+++ configure/load_init_jod.sh
@@ -0,0 +1,3 @@
+!#/bin/bash
+python3 upload_s3.py
+python3 dio-live-wordcount-teste.py -r emr s3://<bucket-name>/datalake-dio/data/sherlock.txt --cloud-output-dir=s3://<bucket-name>/<dir-output-name>/ --cloud-tmp-dir=s3://<bucket-name>/<dir-temp-name>/
